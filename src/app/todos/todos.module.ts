65bff3900e08297c2ea2216c553e7c5d22bcae71
Decclan <dec.evan11@gmail.com>
2022-03-11 11:18:07 +0000

    todos-module-start

---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core'
+
+import {TodosComponent} from './components/todos/todos.component'
+
+@NgModule({
+	declarations: [TodosComponent],
+	imports: []
+})
+export class TodosModule {}
